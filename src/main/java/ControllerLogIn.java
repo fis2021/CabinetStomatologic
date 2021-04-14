bf11cbdc36c05ec9fcb2e1c985801d8b625a2ab3
Rosa Flavius <flavius.rosa@student.upt.ro>
2021-04-14 19:55:33 +0300

    Am rezolvat conflictul de controllere :)

---
+++ src/main/java/ControllerLogIn.java
@@ -0,0 +1,33 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerLogIn{
+
+    private Stage window1;
+    private Scene scene;
+    private Parent root;
+
+
+
+    @FXML
+    Button LOGINButton;
+
+    public void switchToMainPage(MouseEvent event) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        window1 = (Stage)LOGINButton.getScene().getWindow();
+        window1.setScene(new Scene(root));
+    }
+
+
+}
